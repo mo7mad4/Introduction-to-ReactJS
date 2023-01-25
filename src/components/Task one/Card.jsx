18edfb6e2295c357d0baaabd462967228dc7b8b5
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2023-01-25 16:58:52 +0200

    Adding code part 2 in lesson two and create assament

--- src/components/Task one/Card.jsx
+++ src/components/Task one/Card.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import './style.css'
 
-const Card = ({title,img,body}) => {
+// function beasd component
+
+const Card = ({ title, img, body, actions, children }) => {
   return (
     <div className='containers'>
       <div className="containerTeyps">
         <h3>{title}</h3>
         <img src={img} alt="imgTask" />
-        <p>{ body }</p>    
+        {children}
+        <p>{body}</p>
+        {actions ? <div className="action-section">{actions}</div> : ""}
+        {/* {actions && <div className="action-section">{actions}</div>} */}
       </div>
     </div>
   )
 }
+
+// class beasd component
+// class Card extends React.Component{
+//   render() {
+//     const { title, img, body } = this.props;
+//     return (
+//           <div className='containers'>
+//             <div className="containerTeyps">
+//               <h3>{title}</h3>
+//               <img src={img} alt="imgTask" />
+//               <p>{ body }</p>
+//             </div>
+//           </div>
+
+//     )
+//   }
+// }
 
 export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header/Header';
 import Card from "../components/Task one/Card";
 import img1 from "../images/empty-cart.png";
 import img2 from "../images/profile.png";
+import img3 from "../images/categories.png";
 class HomePage extends Component {
     render() {
         return (
@@ -20,18 +21,41 @@ class HomePage extends Component {
                     <Button title="Add new product" />
                     <Button />
                 </section>
-                <Card
-                    title="empty-cart"
-                    img={img1}
-                    body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
-            sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
-                />
-                <Card
-                    title="profile"
-                    img={img2}
-                    body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
-            sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
-                />
+                <section>
+                    <h4>Section 3</h4>
+
+                    <Card
+                        title="Card 1 - Empty-cart"
+                        img={img1}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                    />
+                    <Card
+                        title="Card 2 - Profile"
+                        img={img2}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                    />
+                    <Card
+                        title="Card 3 - Categories"
+                        img={img3}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                    />
+                </section>
+                <section>
+                    <h4>Section 4</h4>
+                    <Card
+                        title="Card 3 - Categories"
+                        img={img3}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                        actions={<Button title='action button' isActive />}
+                    >
+                        <h2 style={{textAlign:'left'}}>Section</h2>
+                        <Button title="Button" />
+                    </Card>
+                </section>
                 <Footer />
             </div>
         );
