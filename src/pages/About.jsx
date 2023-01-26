a6f180801f06ac84ad35cfaa6a9b7303bf79aa2c
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2023-01-26 15:59:14 +0200

    Adding code part 3 in lesson two and create Container component and use in pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,17 @@
 import { Component } from 'react';
+import Container from '../components/Container';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 class AboutPage extends Component {
     render() {
         return (
-            <div className='container'>
-                <Header myPage="about"/>
-                <h2>About Page</h2>
-                <Footer />
-            </div>
+            <>
+                <Header myPage="about" />
+                <Container>
+                    <h2>About Page</h2>
+                    <Footer />
+                </Container>
+            </>
         );
     }
 }
