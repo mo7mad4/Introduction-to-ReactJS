ead83030bf9bc9ec48a0856426bbe9dfd580438b
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2023-02-28 17:16:48 +0200

    Adding part one in this section postman collection & life cycle in deep & ref

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,70 +5,85 @@ import img1 from "../images/empty-cart.png";
 import img2 from "../images/profile.png";
 import img3 from "../images/categories.png";
 import Container from '../components/Container';
+import axios from 'axios';
+import { API_URL } from '../config/api';
 class HomePage extends Component {
-    render() {
-        return (
-            <>
-                <h2>Home Page</h2>
-                <Container className="my-container">
-                    <section>
-                        <h4>Section 1</h4>
-                        <Button title="Click me" isActive />
-                    </section>
-                </Container>
-
-                <Container>
-                    <section>
-                        <h4>Section 2</h4>
-                        <Button title="Add new product" />
-                        <Button />
-                    </section>
-                </Container>
-
-                <Container>
-                    <section>
-                        <h4>Section 3</h4>
-
-                        <Card
-                            title="Card 1 - Empty-cart"
-                            img={img1}
-                            body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
-                            sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
-                        />
-                        <Card
-                            title="Card 2 - Profile"
-                            img={img2}
-                            body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
-                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
-                        />
-                        <Card
-                            title="Card 3 - Categories"
-                            img={img3}
-                            body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
-                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
-                        />
-                    </section>
-                </Container>
-
-                <Container>
-                    <section>
-                        {/* Children we use open and close tag " <Card> </Card> " and we use in Component Card put in props Children  */}
-                        <h4>Section 4</h4>
-                        <Card
-                            title="Card 3 - Categories"
-                            img={img3}
-                            body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
-                        sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
-                            actions={<Button title='action button' isActive />}
-                        >
-                            <h2 style={{ textAlign: 'left' }}>Section</h2>
-                            <Button title="Button" />
-                        </Card>
-                    </section>
-                </Container>
-            </>
-        );
+    state = {
+        isMounted: true
     }
+
+    componentDidMount() {
+       const token = localStorage.getItem('token')
+        axios.get(`${API_URL}user/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    }
+
+render() {
+    return (
+        <>
+            <h2>Home Page</h2>
+            <Container className="my-container">
+                <section>
+                    <h4>Section 1</h4>
+                    <Button title="Click me" isActive />
+                </section>
+            </Container>
+
+            <Container>
+                <section>
+                    <h4>Section 2</h4>
+                    <Button title="Add new product" />
+                    <Button />
+                </section>
+            </Container>
+
+            <Container>
+                <section>
+                    <h4>Section 3</h4>
+
+                    <Card
+                        title="Card 1 - Empty-cart"
+                        img={img1}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                            sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                    />
+                    <Card
+                        title="Card 2 - Profile"
+                        img={img2}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                    />
+                    <Card
+                        title="Card 3 - Categories"
+                        img={img3}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                             sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                    />
+                </section>
+            </Container>
+
+            <Container>
+                <section>
+                    {/* Children we use open and close tag " <Card> </Card> " and we use in Component Card put in props Children  */}
+                    <h4>Section 4</h4>
+                    <Card
+                        title="Card 3 - Categories"
+                        img={img3}
+                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur velit eveniet sit modi vel placeat unde, est
+                        sunt consequatur asperiores error nihil incidunt odio ut dolore numquam. Quis, saepe ipsum."
+                        actions={<Button title='action button' isActive />}
+                    >
+                        <h2 style={{ textAlign: 'left' }}>Section</h2>
+                        <Button title="Button" />
+                    </Card>
+                </section>
+            </Container>
+        </>
+    );
+}
 }
 
 export default HomePage;
